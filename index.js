ff4d3fd5fa626e42efa4ab80be70b5a41f514c49
kelvin porbeni <kelvinporbeni456@gmail.com>
2022-08-25 16:44:18 +0100

    updated script

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ function digitalClock() {
     let h = time.getHours();
     let m = time.getMinutes();
     let s = time.getSeconds();
-    let ampm = 'PM';
+    let ampm = "AM";
 
-    if (h > 12) {
+    if (h > 24) {
         h = h - 12;
-        ampm = "PM";
-    }
+    } 
 
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
+    ampm = h >= 12? "PM":"AM";
 
     
     hourE1.innerHTML = h;
     minuteE1.innerHTML = m;
     secondE1.innerHTML = s;
-    ampmE1, (innerHTML = ampm)
+    ampmE1.innerHTML = ampm;
+    
     setTimeout(()=> {
         digitalClock()
     }, 1000)
+  
 }
 digitalClock();
